8ec71b6c4de59ca52f842910a15de8ae2c30e807
Mehak Sharma <mehak.cse24@sbjit.edu.in>
2026-02-23 12:22:54 +0530

    Practical 6

---
+++ Practical_6/lab2.py
@@ -0,0 +1,11 @@
+# Function definition
+def convert_to_upper(text):
+    return text.upper()
+
+# Take input
+line = input("Enter a sentence: ")
+
+# Call function
+result = convert_to_upper(line)
+
+print("Output:", result)
